1d29caf760703e5c7acb960408eae2dd1cb00eaa
benhassenwael <44639258+benhassenwael@users.noreply.github.com>
2021-04-04 22:04:39 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,6 +4,7 @@
 file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
   line  => '    PasswordAuthentication no',
+  match => 'PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
