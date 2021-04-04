0d25cf1dd4bc704dea6dd924d8c6a330e61a36b7
benhassenwael <44639258+benhassenwael@users.noreply.github.com>
2021-04-04 22:29:01 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,15 @@
 # without typing a password
 
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => '    PasswordAuthentication no',
-  match => '    PasswordAuthentication.*',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  match   => '    PasswordAuthentication.*',
+  replace => true
 }
 
 file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => '    IdentityFile ~/.ssh/holberton',
-  match => '    IdentityFile.*',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  match   => '    IdentityFile.*',
+  replace => true
 }
