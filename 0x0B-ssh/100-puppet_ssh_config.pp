46650d10dd6b9c5761123ca2e132f0ad4048b126
benhassenwael <greenw2012@gmail.com>
2021-04-04 21:43:22 +0000

    -

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # making changes to config file to allow ssh connection
 
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => '    PasswordAuthentication no',
-  match => '    PasswordAuthentication.*'
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true
 }
 
 file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => '    IdentityFile ~/.ssh/holberton',
-  match => '    IdentityFile.*'
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true
 }
